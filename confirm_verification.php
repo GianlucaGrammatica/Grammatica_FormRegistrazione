2005a821f8cc8e3fc6256e6e8c2d961d93b90223
Gianluca Grammatica <147130877+GianlucaGrammatica@users.noreply.github.com>
2025-11-08 08:36:30 +0100

    Update confirm_verification.php

--- confirm_verification.php
+++ confirm_verification.php
@@ -1,1 +1,22 @@
 <?php
+require_once "Database.php";
+
+$title = "Sign Up";
+//$errors = [];
+
+if(!empty($_GET)){
+  $token = $_GET["token"];
+  
+   $pdo = Database::getInstance()->getConnection();
+
+
+    // Controlo se l'username esiste
+    $stmt = $pdo->prepare("SELECT * FROM utenti WHERE verification_token = :token");
+    $stmt->execute(['token' => $token]);
+    $utente = $stmt->fetch();
+
+  if($utente){
+    $stmt = $pdo->prepare("UPDATE utenti SET email_verified = true WHERE id = ?");
+    $stmt->execute([$utente["id]]);
+  }
+}
